4662099ef5e3f11dddd8a02901d6cb5f81543c5c
animesh3640 <karne.ani@gmail.com>
2023-06-20 17:20:41 +0530

    f

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import Parent from "./Parent";
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Parent>
+          <h2>This is the content of my website.</h2>
+        </Parent>
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+const Footer =()=>{
+    return (
+        <div className="footer">
+            <h2>&copy; 2023 My Website. All rights reserved.</h2>
+        </div>
+    )
+}
+export default Footer;
